74ce7f4a377aacc30284436f2eab2309634406a0
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-07-03 17:44:37 +0530

    NetflixProject

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Cards';
+import Sdata from './Sdata';
+
+function ncard(val) {
+	return(
+		<Card imgsrc= {val.imgsrc}
+		title={val.title}
+		sname={val.sname}
+		link={val.link}
+		/>
+		);
+}
+ReactDOM.render(
+	<>
+	<h1 className="heading_style">List of top 5 Netflix series </h1>
+	{Sdata.map(ncard)}
+
+	{/*Fat arrow func {Sdata.map((val) =>
+{return(<Card imgsrc={val.imgsrc}
+	title={val.title}
+	sname={val.sname}
+	link={val.link}
+	/>);
+})} */ }
+</>
+, document.getElementById("root")
+	)
